be0f72d1f10a3b10e46f0612e979c26d61c764d2
hhertout <hugues.hertout@gmail.com>
2023-01-11 19:36:51 +0100

    security home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,6 +11,11 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        /** @var \App\Entity\User $user */
+        $user = $this->getUser();
+        if ($user === null) {
+            return $this->redirectToRoute('app_login');
+        }
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
