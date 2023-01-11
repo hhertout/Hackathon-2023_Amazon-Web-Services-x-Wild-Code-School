e27f5774deae2edac1cacf9e554c0578d5758a5b
hhertout <hugues.hertout@gmail.com>
2023-01-11 19:24:06 +0100

    fix

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,19 +4,14 @@ namespace App\Form;
 
 use App\Entity\User;
 use App\Entity\Company;
-use Doctrine\ORM\Mapping\Entity;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class RegistrationFormType extends AbstractType
 {
